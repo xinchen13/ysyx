9ecab9a70517e087917f130b063b252532959c98
tracer-ysyx <tracer@ysyx.org>
2024-08-07 10:41:04 +0800

    > compile NEMU ysyx_22040000 xinchen Linux xinchen-vm-ubuntu2204 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:41:04 up  1:21,  1 user,  load average: 0.65, 0.44, 0.47

--- github-up.sh
+++ github-up.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 source tenv
 
-for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
-    git branch --track ${branch#remotes/xinchen/} $branch
+for remote_branch in `git branch -a | grep remotes | grep -v HEAD`; do
+    local_branch=${remote_branch#remotes/xinchen/}
+    if [ -z "$(git branch --list $local_branch)" ]; then
+        git branch --track $local_branch $remote_branch
+    else
+        echo "Local branch $local_branch is already tracking a remote branch. Skipping."
+    fi
 done
 
 git pull --all
