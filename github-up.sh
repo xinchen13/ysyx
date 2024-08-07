af8adc83c0c4accef8ca44be3ddae7a02e11041c
xinchen <xinchen.sakura@gmail.com>
2024-08-07 10:08:53 +0800

    modified github-up

--- github-up.sh
+++ github-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source tenv
 
-for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
+for branch in `git branch -a | grep remotes | grep -v HEAD`; do
     git branch --track ${branch#remotes/xinchen/} $branch
 done
 
