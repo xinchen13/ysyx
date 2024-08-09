fdfc0eda5f052dad044af668f268ee3e046cf1d0
tracer-ysyx <tracer@ysyx.org>
2024-08-09 13:03:51 +0800

    > compile NEMU ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:03:51 up  3:31,  1 user,  load average: 0.52, 0.30, 0.28

--- nemu/src/monitor/sdb/sdb.h
+++ nemu/src/monitor/sdb/sdb.h
@@ -20,4 +20,27 @@
 
 word_t expr(char *e, bool *success);
 
+// define WP type 
+typedef struct watchpoint {
+    int NO;
+    struct watchpoint *next;
+
+    /* TODO: Add more members if necessary */
+    char WP_expr[64]; // the expression to watch
+    word_t val;       // val = expr(WP_expr)
+
+} WP;
+
+// get a new watchpoints in free_ and return
+WP* new_wp(char* WP_expr, bool *success);
+
+// free a watchpoint in head by num
+void free_wp(int num);
+
+// display all of the wp
+void watchpoint_display();
+
+// check all the wp, return whether changed
+int check_watchpoint();
+
 #endif
