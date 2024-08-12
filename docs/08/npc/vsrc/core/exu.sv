95cfc054cae17c3bda5acf54a2c7c40abad7b65a
xinchen <xinchen.sakura@gmail.com>
2024-08-12 23:34:39 +0800

    update npc-addi

--- docs/08/npc/vsrc/core/exu.sv
+++
@@ -1,9 +0,0 @@
-`include "../inc/defines.svh"
-
-module exu (
-    input logic [`DATA_BUS] src1,
-    input logic [`DATA_BUS] src2,
-    output logic [`DATA_BUS] reg_wdata
-);
-    assign reg_wdata = src1 + src2;
-endmodule
