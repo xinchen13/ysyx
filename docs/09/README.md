6022e74c82ebe9b66433fa0dc063fa6bc480f506
tracer-ysyx <tracer@ysyx.org>
2024-08-13 16:24:43 +0800

    > compile NEMU ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:43 up 1 day,  6:29,  1 user,  load average: 1.62, 1.12, 0.92

--- docs/09/README.md
+++ docs/09/README.md
@@ -63,3 +63,6 @@ itrace (instruction trace), 它可以记录客户程序执行的每一条指令.
 - 指令环形缓冲区的初始化放在`$NEMU_HOME/src/monitor/monitor.c`中的`init_monitor()`函数中进行, 当没有配置itrace时不进行初始化节约时间开销
 - 在cpu执行过程中(即运行`cpu_exec()`), `trace_and_difftest()`函数通过`CONFIG_ITRACE`宏配置开关iringbuf的读写; 通过把128位的`s->logbuf`写入iringbuf来维护指令执行历史; 当遇到`nemu_state.state = NEMU_ABORT`时，输出缓冲区
 - 把`inst.c`中的指令注释后运行nemu，验证实现正确性
+
+## 内存访问的踪迹 - mtrace
+实现mtrace只需要在`paddr_read()`和`paddr_write()`中进行记录即可. 可以自行定义mtrace输出的格式. 和最后只输出一次的iringbuf不同, 程序一般会执行很多访存指令, 这意味着开启mtrace将会产生大量的输出, 因此最好可以在不需要的时候关闭mtrace
--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,14 +51,26 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  out_of_bound(addr);
-  return 0;
+    // memory trace
+    #ifdef CONFIG_MTRACE
+        if (addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) {
+            Log(" read, len = %3d (bytes), addr = " FMT_WORD, len, addr);
+        }
+    #endif
+    if (likely(in_pmem(addr))) return pmem_read(addr, len);
+    IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+    out_of_bound(addr);
+    return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  out_of_bound(addr);
+    // memory trace
+    #ifdef CONFIG_MTRACE
+        if (addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) {
+            Log("write, len = %3d (bytes), addr = " FMT_WORD " data = " FMT_WORD, len, addr, data);
+        }
+    #endif 
+    if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+    IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+    out_of_bound(addr);
 }
