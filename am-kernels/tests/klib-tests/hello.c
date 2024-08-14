f0b47e3e5e3b276c85968498a7c0cf1e7dc1e87d
tracer-ysyx <tracer@ysyx.org>
2024-08-14 11:18:22 +0800

    > compile NEMU ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:18:22 up  2:06,  1 user,  load average: 1.32, 0.75, 0.75

--- am-kernels/tests/klib-tests/hello.c
+++ am-kernels/tests/klib-tests/hello.c
@@ -3,12 +3,9 @@
 #include <klib.h>
 
 int main(const char *args) {
-  const char *fmt =
-    "Hello, AbstractMachine!\n"
-    "mainargs = '%'.\n";
-
-  for (const char *p = fmt; *p; p++) {
-    (*p == '%') ? putstr(args) : putch(*p);
-  }
-  return 0;
+    const char *fmt =
+        "Hello, AbstractMachine!\n"
+        "mainargs = '%'.\n";
+    printf("%s",fmt);
+    return 0;
 }
