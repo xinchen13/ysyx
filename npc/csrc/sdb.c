fc13037c82d8073cf29e3e32adb16a18cf505634
tracer-ysyx <tracer@ysyx.org>
2024-08-16 22:53:42 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:53:42 up  3:47,  1 user,  load average: 0.12, 0.21, 0.22

--- npc/csrc/sdb.c
+++ npc/csrc/sdb.c
@@ -14,7 +14,7 @@ static char* rl_gets() {
         line_read = NULL;
     }
 
-    line_read = readline("(nemu) ");
+    line_read = readline("(npc) ");
 
     if (line_read && *line_read) {
         add_history(line_read);
@@ -191,31 +191,31 @@ void sdb_mainloop() {
         return;
     }
 
-  for (char *str; (str = rl_gets()) != NULL; ) {
-    char *str_end = str + strlen(str);
+    for (char *str; (str = rl_gets()) != NULL; ) {
+        char *str_end = str + strlen(str);
 
-    /* extract the first token as the command */
-    char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
+        /* extract the first token as the command */
+        char *cmd = strtok(str, " ");
+        if (cmd == NULL) { continue; }
 
-    /* treat the remaining string as the arguments,
-     * which may need further parsing
-     */
-    char *args = cmd + strlen(cmd) + 1;
-    if (args >= str_end) {
-      args = NULL;
+        /* treat the remaining string as the arguments,
+        * which may need further parsing
+        */
+        char *args = cmd + strlen(cmd) + 1;
+        if (args >= str_end) {
+            args = NULL;
+        }
+
+        int i;
+        for (i = 0; i < NR_CMD; i ++) {
+        if (strcmp(cmd, cmd_table[i].name) == 0) {
+            if (cmd_table[i].handler(args) < 0) { return; }
+            break;
+        }
+        }
+
+        if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
     }
-
-    int i;
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
-        break;
-      }
-    }
-
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-  }
 }
 
 void init_sdb() {
