518c5b43be286601cb3a2548c5b32ac1184dfa92
xinchen <xinchen.sakura@gmail.com>
2024-08-16 21:19:28 +0800

    add log to npc

--- npc/csrc/monitor.c
+++ npc/csrc/monitor.c
@@ -1,23 +1,42 @@
-#include "monitor.h"
 #include "common.h"
 #include "paddr.h"
 #include "host.h"
 
 static char *img_file = NULL;   // image file
+static char *log_file = NULL;
+
+extern FILE *log_fp;
+
+static void welcome() {
+    Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
+    Log("Build time: %s, %s", __TIME__, __DATE__);
+    printf("Welcome to %s-npc!\n", ANSI_FMT("riscv32e", ANSI_FG_YELLOW ANSI_BG_RED));
+    printf("For help, type \"help\"\n");
+}
+
+static void init_log(const char *log_file) {
+    log_fp = stdout;
+    if (log_file != NULL) {
+        FILE *fp = fopen(log_file, "w");
+        Assert(fp, "Can not open '%s'", log_file);
+        log_fp = fp;
+    }
+    Log("Log is written to %s", log_file ? log_file : "stdout");
+}
 
 static long load_img() {
-    if (img_file == NULL) {
-        printf("No image is given. Use the default build-in image.\n");
-        return 0;
-    }
+    // if (img_file == NULL) {
+    //     Assert("No image is given. Use the default build-in image.\n");
+    //     return 0;
+    // }
 
     FILE *fp = fopen(img_file, "rb");
-    assert(fp);
+    Assert(fp, "Can not open '%s'", img_file);
 
     fseek(fp, 0, SEEK_END);
     long size = ftell(fp);
 
-    printf("load_img(): The image is %s, size = %ld\n", img_file, size);
+    Log("load_img(): The image is %s, size = %ld\n", img_file, size);
 
     fseek(fp, 0, SEEK_SET);
     int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
@@ -29,16 +48,19 @@ static long load_img() {
 
 static int parse_args(int argc, char *argv[]) {
     const struct option table[] = {
+        {"log"      , required_argument, NULL, 'l'},
         {"img"      , required_argument, NULL, 'i'},
         {"help"     , no_argument      , NULL, 'h'},
         {0          , 0                , NULL,  0 },
     };
     int o;
-    while ((o = getopt_long(argc, argv, "-hi:", table, NULL)) != -1) {
+    while ((o = getopt_long(argc, argv, "-hl:i:", table, NULL)) != -1) {
         switch (o) {
+            case 'l': log_file = optarg; break;
             case 'i': img_file = optarg; break;
             default:
                 printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+                printf("\t-l,--log=FILE           output log to FILE\n");
                 printf("\t-i,--img=FILE read img file\n");
                 printf("\n");
                 exit(0);
@@ -54,6 +76,9 @@ void init_monitor(int argc, char *argv[]) {
     /* Parse arguments. */
     parse_args(argc, argv);
 
+    /* Open the log file. */
+    init_log(log_file);
+
     /* Initialize memory. */
     init_mem();
 
@@ -61,5 +86,5 @@ void init_monitor(int argc, char *argv[]) {
     long img_size = load_img();
 
     /* Display welcome message. */
-    // welcome();
+    welcome();
 }
--- npc/csrc/monitor.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MONITOR_H__
-#define __MONITOR_H__
-
-#include "common.h"
-
-void init_monitor(int argc, char *argv[]);
-
-#endif
