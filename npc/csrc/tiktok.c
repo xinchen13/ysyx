bc43113e70062db37af9cc1b59ac3045947367ac
tracer-ysyx <tracer@ysyx.org>
2024-08-29 11:55:39 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:55:39 up 3 days,  2:55,  1 user,  load average: 2.10, 1.53, 1.43

--- npc/csrc/tiktok.c
+++ npc/csrc/tiktok.c
@@ -73,6 +73,7 @@ static void trace_and_difftest() {
         if ((uint32_t)dut->rootp->xcore__DOT__alu_result == (0xa00003f8)) {
             // difftest_skip_ref();
             printf("a%u, adfafsf\n",dut->rootp->xcore__DOT__alu_result);
+            difftest_skip_ref();
         }
         difftest_step(core.pc, core.pc);    // 第二个参数暂时没用
     #endif
