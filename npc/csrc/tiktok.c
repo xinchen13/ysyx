4728c0789e095cdfe1c1619caa50a05c7326d6c9
tracer-ysyx <tracer@ysyx.org>
2024-08-31 17:21:34 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:21:34 up 14:57,  1 user,  load average: 1.76, 1.32, 1.23

--- npc/csrc/tiktok.c
+++ npc/csrc/tiktok.c
@@ -135,7 +135,10 @@ static void exec_once() {
     contextp->timeInc(1); // time + 1
 
     // update regs in monitor
-    isa_reg_update();
+    if (dut->rootp->xcore__DOT__pc_valid) {
+        isa_reg_update();
+    }
+    
 
     #ifdef CONFIG_ITRACE
         char *p = logbuf;
