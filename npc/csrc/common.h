a8c692b4fbaa8c22fd006297d1dad2050a58982b
tracer-ysyx <tracer@ysyx.org>
2024-09-01 11:03:04 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:04 up 19:14,  1 user,  load average: 1.86, 1.77, 1.36

--- npc/csrc/common.h
+++ npc/csrc/common.h
@@ -8,9 +8,9 @@
 #define CONFIG_RVE 1
 // #define CONFIG_WATCHPOINT 1
 #define CONFIG_ITRACE 1
-// #define CONFIG_FTRACE 1
+#define CONFIG_FTRACE 1
 // #define CONFIG_MTRACE 1
-#define CONFIG_DIFFTEST 1
+// #define CONFIG_DIFFTEST 1
 
 #if defined(CONFIG_ITRACE) || defined(CONFIG_FTRACE) || defined(CONFIG_MTRACE)
     #define CONFIG_TRACE 1
