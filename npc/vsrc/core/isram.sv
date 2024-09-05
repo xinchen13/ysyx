5489042960da4d71a631a851abd9af9ce7593388
tracer-ysyx <tracer@ysyx.org>
2024-09-05 17:02:35 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:02:35 up 3 days,  8:54,  1 user,  load average: 2.02, 1.61, 1.16

--- npc/vsrc/core/isram.sv
+++ npc/vsrc/core/isram.sv
@@ -29,85 +29,104 @@ module isram (
     input logic bready
 );
 
-    logic [2:0] lfsr;
+    logic done;
+    assign done = 1'b1;
+
+    assign arready = !arvalid || (done && rready);
+    assign rvalid = arvalid & done;
 
     // DPI-C: pmem_read, pmem_write
     import "DPI-C" function int dpic_pmem_read(input int raddr);
 
-    localparam [1:0] IDLE = 2'b00;
-    localparam [1:0] READ = 2'b01;
-
-    logic [1:0] state;
-    logic [1:0] next_state;
-
     // sram
-    logic sram_ack;                     // SRAM读取完成信号
-    logic [2:0] sram_wait_counter;      // 用于模拟不确定延迟的计数器
-
-    // state reg
-    always @ (posedge clk) begin
-        if (!rst_n) begin
-            state <= IDLE;
-        end 
-        else begin
-            state <= next_state;
-        end
-    end
-
-
-    assign arready = (state == IDLE) ? 1'b1 : 1'b0;
-    assign rresp = 2'b00;
-    assign rvalid = ((state == READ) && sram_ack) ? 1'b1 : 1'b0;
-
-    // trans logic
     always @ (*) begin
-        next_state = state;
-        case (state)
-            IDLE: begin
-                if (arvalid && arready) begin
-                    next_state = READ;  // 转移到READ状态
-                end
-            end
-            READ: begin
-                if (rvalid && rready) begin
-                    next_state = IDLE;  // 数据传输完成，回到IDLE状态
-                end
-            end
-            default: begin
-                next_state = IDLE;
-            end
-        endcase
-    end
-
-
-    always @(posedge clk) begin
-        if (!rst_n) begin
-            sram_ack  <= 1'b0;
-            sram_wait_counter <= 3'b000;  // 初始化等待计数器
-            rdata <= `INST_NOP;
-            lfsr <= 3'b001;
-        end 
+        if (arvalid) begin
+            rdata = dpic_pmem_read(araddr);
+        end
         else begin
-            case (state)
-                IDLE: begin
-                    sram_wait_counter <= 3'b000; // 重置等待计数器
-                    sram_ack <= 1'b0;
-                    lfsr <= {lfsr[1:0], lfsr[2] ^ lfsr[1]}; // LFSR反馈多项式, 伪随机延迟
-                end
-                READ: begin
-                    if (sram_wait_counter == lfsr) begin  // 模拟读取延迟
-                        rdata <= dpic_pmem_read(araddr);  // 从SRAM读取数据
-                        sram_ack   <= 1'b1;  // 读取完成信号
-                    end 
-                    else begin
-                        sram_ack <= 1'b0;
-                        sram_wait_counter <= sram_wait_counter + 1;
-                    end
-                end
-                default: begin
-                end
-            endcase
+            rdata = `INST_NOP;
         end
     end
+
+    // logic [2:0] lfsr;
+
+    // // DPI-C: pmem_read, pmem_write
+    // import "DPI-C" function int dpic_pmem_read(input int raddr);
+
+    // localparam [1:0] IDLE = 2'b00;
+    // localparam [1:0] READ = 2'b01;
+
+    // logic [1:0] state;
+    // logic [1:0] next_state;
+
+    // // sram
+    // logic sram_ack;                     // SRAM读取完成信号
+    // logic [2:0] sram_wait_counter;      // 用于模拟不确定延迟的计数器
+
+    // // state reg
+    // always @ (posedge clk) begin
+    //     if (!rst_n) begin
+    //         state <= IDLE;
+    //     end 
+    //     else begin
+    //         state <= next_state;
+    //     end
+    // end
+
+
+    // assign arready = (state == IDLE) ? 1'b1 : 1'b0;
+    // assign rresp = 2'b00;
+    // assign rvalid = ((state == READ) && sram_ack) ? 1'b1 : 1'b0;
+
+    // // trans logic
+    // always @ (*) begin
+    //     next_state = state;
+    //     case (state)
+    //         IDLE: begin
+    //             if (arvalid && arready) begin
+    //                 next_state = READ;  // 转移到READ状态
+    //             end
+    //         end
+    //         READ: begin
+    //             if (rvalid && rready) begin
+    //                 next_state = IDLE;  // 数据传输完成，回到IDLE状态
+    //             end
+    //         end
+    //         default: begin
+    //             next_state = IDLE;
+    //         end
+    //     endcase
+    // end
+
+
+    // always @(posedge clk) begin
+    //     if (!rst_n) begin
+    //         sram_ack  <= 1'b0;
+    //         sram_wait_counter <= 3'b000;  // 初始化等待计数器
+    //         rdata <= `INST_NOP;
+    //         lfsr <= 3'b001;
+    //     end 
+    //     else begin
+    //         case (state)
+    //             IDLE: begin
+    //                 sram_wait_counter <= 3'b000; // 重置等待计数器
+    //                 sram_ack <= 1'b0;
+    //                 lfsr <= {lfsr[1:0], lfsr[2] ^ lfsr[1]}; // LFSR反馈多项式, 伪随机延迟
+    //             end
+    //             READ: begin
+    //                 if (sram_wait_counter == lfsr) begin  // 模拟读取延迟
+    //                     rdata <= dpic_pmem_read(araddr);  // 从SRAM读取数据
+    //                     sram_ack   <= 1'b1;  // 读取完成信号
+    //                 end 
+    //                 else begin
+    //                     sram_ack <= 1'b0;
+    //                     sram_wait_counter <= sram_wait_counter + 1;
+    //                 end
+    //             end
+    //             default: begin
+    //             end
+    //         endcase
+    //     end
+    // end
 
 endmodule
