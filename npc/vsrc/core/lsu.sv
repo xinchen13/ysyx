136d254466b8d8c56837fe7a1898fadffdd3987a
xinchen <xinchen.sakura@gmail.com>
2024-09-06 16:46:12 +0800

    update unfinished lsu axi interface

--- npc/vsrc/core/lsu.sv
+++ npc/vsrc/core/lsu.sv
@@ -4,45 +4,53 @@ module lsu (
     input logic clk,
     input logic rst_n,
     input logic [`INST_DATA_BUS] inst,
+    input logic [`AXI_WSTRB_BUS] wmask,
     input logic [`DATA_BUS] raddr,
     input logic [`DATA_BUS] waddr,
-    input logic [`DATA_BUS] wdata,
+    input logic [`DATA_BUS] ex_wdata,
     input logic wen,
     input logic req,
-    output logic [`DATA_BUS] rdata
+    output logic [`DATA_BUS] lsu_rdata,
+
+    input logic prev_valid,
+    output logic this_ready,
+    input logic next_ready,
+    output logic this_valid,
+
+    // axi-lite interface (master)
+    // AR
+    output logic [`AXI_ADDR_BUS] araddr,
+    output logic arvalid,
+    input logic arready,
+    // R
+    input logic [`AXI_DATA_BUS] rdata,
+    input logic [`AXI_RESP_BUS] rresp,
+    input logic rvalid,
+    output logic rready,
+    // AW
+    output logic [`AXI_ADDR_BUS] awaddr,
+    output logic awvalid,
+    input logic awready,
+    // W
+    output logic [`AXI_DATA_BUS] wdata,
+    output logic [`AXI_WSTRB_BUS] wstrb,
+    output logic wvalid,
+    input logic wready,
+    // B
+    input logic [`AXI_RESP_BUS] bresp,
+    input logic bvalid,
+    output logic bready
 );
 
     logic [6:0] opcode = inst[6:0];
     logic [2:0] funct3 = inst[14:12];
-    logic [`BYTE_BUS] wmask;
-    logic [`DATA_BUS] dmem_rdata_raw;
-    logic [`DATA_BUS] masked_dmem_rdata;
-    logic [4:0] dmem_offset;
-    logic [`DATA_BUS] dmem_rdata_offset;
-    logic [`DATA_BUS] dmem_wdata_offset;
-
-    // DPI-C: pmem_read, pmem_write
-    import "DPI-C" function int dpic_pmem_read(input int raddr);
-    import "DPI-C" function void dpic_pmem_write(input int waddr, input int wdata, input byte wmask);
-
-    // sram
-    always @ (posedge clk) begin
-        if (wen) begin
-            dpic_pmem_write(waddr, dmem_wdata_offset, wmask);
-        end
-    end
-    always @ (*) begin
-        if (req) begin
-            dmem_rdata_raw = dpic_pmem_read(raddr);
-        end
-        else begin
-            dmem_rdata_raw = 0;
-        end
-    end
 
     // rmask
-    assign dmem_offset = {3'b0, raddr[1:0]};
-    assign dmem_rdata_offset = dmem_rdata_raw >> (dmem_offset << 3);
+    logic [4:0] roffset;
+    logic [`DATA_BUS] dmem_rdata_offset;
+    logic [`DATA_BUS] masked_dmem_rdata;
+    assign roffset = {3'b0, raddr[1:0]};
+    assign dmem_rdata_offset = rdata >> (roffset << 3);
     always @ (*) begin
         case (opcode)
             `I_LOAD_TYPE_OPCODE: begin
@@ -72,35 +80,29 @@ module lsu (
             end
         endcase
     end
+    assign lsu_rdata = masked_dmem_rdata;
 
-    assign rdata = masked_dmem_rdata;
+    // wdata offset
+    logic [4:0] woffset;
+    logic [`DATA_BUS] wdata_offset;
+    assign woffset = {3'b0, waddr[1:0]};
+    assign wdata_offset = ex_wdata << (woffset << 3);
 
-    // wmask
-    always @ (*) begin
-        case (opcode)
-            `S_TYPE_OPCODE: begin
-                case (funct3)
-                    3'b000: begin
-                        wmask = 8'b00000001 << dmem_offset;
-                    end
-                    3'b001: begin
-                        wmask = 8'b00000011 << dmem_offset;
-                    end
-                    3'b010: begin
-                        wmask = 8'b00001111 << dmem_offset;
-                    end
-                    default: begin
-                        wmask = 8'b00000000;
-                    end
-                endcase
-            end
-            default: begin
-                wmask = 8'b00000000;
-            end
-        endcase
-    end
-    
-    assign dmem_wdata_offset = wdata << (dmem_offset << 3);
+    // wmask offset
+    logic [`AXI_WSTRB_BUS] wmask_offset;
+    assign wmask_offset = wmask << waddr[1:0];
 
+    // axi signals
+    assign araddr = raddr;
+    assign arvalid = req & prev_valid;
+    assign rready = next_ready;
+    assign awaddr = waddr;
+    assign awvalid = wen & prev_valid;
+    assign wdata = wdata_offset;
+    assign wstrb = wmask_offset;
+    assign wvalid = prev_valid;
+    assign this_ready = arready & awready;
+    assign this_valid = bvalid | rvalid;
+    assign bready = next_ready;
 
 endmodule
