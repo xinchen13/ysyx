67b39099eda39f339ec788f7bee9707e7f3c421d
tracer-ysyx <tracer@ysyx.org>
2024-09-16 21:54:03 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:54:03 up 1 day,  2:53,  1 user,  load average: 0.18, 0.21, 0.14

--- npc/vsrc/soc_top.sv
+++ npc/vsrc/soc_top.sv
@@ -8,29 +8,46 @@ module soc_top (
 /*AUTOWIRE*/
 // Beginning of automatic wires (for undeclared instantiated-module outputs)
 logic [`AXI_ADDR_BUS]	fetch_araddr;		// From xcore_u0 of xcore.v
-logic			fetch_arready;		// From isram_u0 of isram.v
+logic			fetch_arready;		// From arbiter_u0 of arbiter.v
 logic			fetch_arvalid;		// From xcore_u0 of xcore.v
-logic [`AXI_DATA_BUS]	fetch_rdata;		// From isram_u0 of isram.v
+logic [`AXI_DATA_BUS]	fetch_rdata;		// From arbiter_u0 of arbiter.v
 logic			fetch_rready;		// From xcore_u0 of xcore.v
-logic [`AXI_RESP_BUS]	fetch_rresp;		// From isram_u0 of isram.v
-logic			fetch_rvalid;		// From isram_u0 of isram.v
+logic [`AXI_RESP_BUS]	fetch_rresp;		// From arbiter_u0 of arbiter.v
+logic			fetch_rvalid;		// From arbiter_u0 of arbiter.v
 logic [`AXI_ADDR_BUS]	lsu_araddr;		// From xcore_u0 of xcore.v
-logic			lsu_arready;		// From dsram_u0 of dsram.v
+logic			lsu_arready;		// From arbiter_u0 of arbiter.v
 logic			lsu_arvalid;		// From xcore_u0 of xcore.v
 logic [`AXI_ADDR_BUS]	lsu_awaddr;		// From xcore_u0 of xcore.v
-logic			lsu_awready;		// From dsram_u0 of dsram.v
+logic			lsu_awready;		// From arbiter_u0 of arbiter.v
 logic			lsu_awvalid;		// From xcore_u0 of xcore.v
 logic			lsu_bready;		// From xcore_u0 of xcore.v
-logic [`AXI_RESP_BUS]	lsu_bresp;		// From dsram_u0 of dsram.v
-logic			lsu_bvalid;		// From dsram_u0 of dsram.v
-logic [`AXI_DATA_BUS]	lsu_rdata;		// From dsram_u0 of dsram.v
+logic [`AXI_RESP_BUS]	lsu_bresp;		// From arbiter_u0 of arbiter.v
+logic			lsu_bvalid;		// From arbiter_u0 of arbiter.v
+logic [`AXI_DATA_BUS]	lsu_rdata;		// From arbiter_u0 of arbiter.v
 logic			lsu_rready;		// From xcore_u0 of xcore.v
-logic [`AXI_RESP_BUS]	lsu_rresp;		// From dsram_u0 of dsram.v
-logic			lsu_rvalid;		// From dsram_u0 of dsram.v
+logic [`AXI_RESP_BUS]	lsu_rresp;		// From arbiter_u0 of arbiter.v
+logic			lsu_rvalid;		// From arbiter_u0 of arbiter.v
 logic [`AXI_DATA_BUS]	lsu_wdata;		// From xcore_u0 of xcore.v
-logic			lsu_wready;		// From dsram_u0 of dsram.v
+logic			lsu_wready;		// From arbiter_u0 of arbiter.v
 logic [`AXI_WSTRB_BUS]	lsu_wstrb;		// From xcore_u0 of xcore.v
 logic			lsu_wvalid;		// From xcore_u0 of xcore.v
+logic [`AXI_ADDR_BUS]	sram_araddr;		// From arbiter_u0 of arbiter.v
+logic			sram_arready;		// From sram_u0 of sram.v
+logic			sram_arvalid;		// From arbiter_u0 of arbiter.v
+logic [`AXI_ADDR_BUS]	sram_awaddr;		// From arbiter_u0 of arbiter.v
+logic			sram_awready;		// From sram_u0 of sram.v
+logic			sram_awvalid;		// From arbiter_u0 of arbiter.v
+logic			sram_bready;		// From arbiter_u0 of arbiter.v
+logic [`AXI_RESP_BUS]	sram_bresp;		// From sram_u0 of sram.v
+logic			sram_bvalid;		// From sram_u0 of sram.v
+logic [`AXI_DATA_BUS]	sram_rdata;		// From sram_u0 of sram.v
+logic			sram_rready;		// From arbiter_u0 of arbiter.v
+logic [`AXI_RESP_BUS]	sram_rresp;		// From sram_u0 of sram.v
+logic			sram_rvalid;		// From sram_u0 of sram.v
+logic [`AXI_DATA_BUS]	sram_wdata;		// From arbiter_u0 of arbiter.v
+logic			sram_wready;		// From sram_u0 of sram.v
+logic [`AXI_WSTRB_BUS]	sram_wstrb;		// From arbiter_u0 of arbiter.v
+logic			sram_wvalid;		// From arbiter_u0 of arbiter.v
 // End of automatics
 
 /*xcore AUTO_TEMPLATE (
@@ -69,100 +86,171 @@ xcore xcore_u0 (/*AUTOINST*/
 		.lsu_bvalid		(lsu_bvalid)); 
 
 
-/*isram AUTO_TEMPLATE (
+/*arbiter AUTO_TEMPLATE (
     .clk(clk),
     .rst_n(rst_n),
-    .araddr(fetch_araddr),
-    .arvalid(fetch_arvalid),
-    .arready(fetch_arready),
-    .rdata(fetch_rdata[`AXI_DATA_BUS]),
-    .rresp(fetch_rresp[`AXI_RESP_BUS]),
-    .rvalid(fetch_rvalid),
-    .rready(fetch_rready),
-    .awaddr(),
-    .awvalid(),
-    .awready(),
-    .wdata(),
-    .wstrb(),
-    .wvalid(),
-    .wready(),
-    .bresp(),
-    .bvalid(),
-    .bready(),
+
+    .m0_araddr(fetch_araddr),
+    .m0_arvalid(fetch_arvalid),
+    .m0_arready(fetch_arready),
+    .m0_rdata(fetch_rdata[`AXI_DATA_BUS]),
+    .m0_rresp(fetch_rresp[`AXI_RESP_BUS]),
+    .m0_rvalid(fetch_rvalid),
+    .m0_rready(fetch_rready),
+    .m0_awaddr(),
+    .m0_awvalid(),
+    .m0_awready(),
+    .m0_wdata(),
+    .m0_wstrb(),
+    .m0_wvalid(),
+    .m0_wready(),
+    .m0_bresp(),
+    .m0_bvalid(),
+    .m0_bready(),
+
+    .m1_araddr(lsu_araddr),
+    .m1_arvalid(lsu_arvalid),
+    .m1_arready(lsu_arready),
+    .m1_rdata(lsu_rdata[`AXI_DATA_BUS]),
+    .m1_rresp(lsu_rresp[`AXI_RESP_BUS]),
+    .m1_rvalid(lsu_rvalid),
+    .m1_rready(lsu_rready),
+    .m1_awaddr(lsu_awaddr),
+    .m1_awvalid(lsu_awvalid),
+    .m1_awready(lsu_awready),
+    .m1_wdata(lsu_wdata[`AXI_DATA_BUS]),
+    .m1_wstrb(lsu_wstrb),
+    .m1_wvalid(lsu_wvalid),
+    .m1_wready(lsu_wready),
+    .m1_bresp(lsu_bresp[`AXI_RESP_BUS]),
+    .m1_bvalid(lsu_bvalid),
+    .m1_bready(lsu_bready),
+
+    .s0_araddr(sram_araddr[`AXI_ADDR_BUS]),
+    .s0_arvalid(sram_arvalid),
+    .s0_arready(sram_arready),
+    .s0_rdata(sram_rdata[`AXI_DATA_BUS]),
+    .s0_rresp(sram_rresp[`AXI_RESP_BUS]),
+    .s0_rvalid(sram_rvalid),
+    .s0_rready(sram_rready),
+    .s0_awaddr(sram_awaddr[`AXI_ADDR_BUS]),
+    .s0_awvalid(sram_awvalid),
+    .s0_awready(sram_awready),
+    .s0_wdata(sram_wdata[`AXI_DATA_BUS]),
+    .s0_wstrb(sram_wstrb[`AXI_WSTRB_BUS]),
+    .s0_wvalid(sram_wvalid),
+    .s0_wready(sram_wready),
+    .s0_bresp(sram_bresp[`AXI_RESP_BUS]),
+    .s0_bvalid(sram_bvalid),
+    .s0_bready(sram_bready),
 );
 */
-isram isram_u0 (/*AUTOINST*/
-		// Outputs
-		.arready		(fetch_arready),	 // Templated
-		.rdata			(fetch_rdata[`AXI_DATA_BUS]), // Templated
-		.rresp			(fetch_rresp[`AXI_RESP_BUS]), // Templated
-		.rvalid			(fetch_rvalid),		 // Templated
-		.awready		(),			 // Templated
-		.wready			(),			 // Templated
-		.bresp			(),			 // Templated
-		.bvalid			(),			 // Templated
-		// Inputs
-		.clk			(clk),			 // Templated
-		.rst_n			(rst_n),		 // Templated
-		.araddr			(fetch_araddr),		 // Templated
-		.arvalid		(fetch_arvalid),	 // Templated
-		.rready			(fetch_rready),		 // Templated
-		.awaddr			(),			 // Templated
-		.awvalid		(),			 // Templated
-		.wdata			(),			 // Templated
-		.wstrb			(),			 // Templated
-		.wvalid			(),			 // Templated
-		.bready			());			 // Templated
+arbiter arbiter_u0 (/*AUTOINST*/
+		    // Outputs
+		    .s0_araddr		(sram_araddr[`AXI_ADDR_BUS]), // Templated
+		    .s0_arvalid		(sram_arvalid),		 // Templated
+		    .s0_rready		(sram_rready),		 // Templated
+		    .s0_awaddr		(sram_awaddr[`AXI_ADDR_BUS]), // Templated
+		    .s0_awvalid		(sram_awvalid),		 // Templated
+		    .s0_wdata		(sram_wdata[`AXI_DATA_BUS]), // Templated
+		    .s0_wstrb		(sram_wstrb[`AXI_WSTRB_BUS]), // Templated
+		    .s0_wvalid		(sram_wvalid),		 // Templated
+		    .s0_bready		(sram_bready),		 // Templated
+		    .m0_arready		(fetch_arready),	 // Templated
+		    .m0_rdata		(fetch_rdata[`AXI_DATA_BUS]), // Templated
+		    .m0_rresp		(fetch_rresp[`AXI_RESP_BUS]), // Templated
+		    .m0_rvalid		(fetch_rvalid),		 // Templated
+		    .m0_awready		(),			 // Templated
+		    .m0_wready		(),			 // Templated
+		    .m0_bresp		(),			 // Templated
+		    .m0_bvalid		(),			 // Templated
+		    .m1_arready		(lsu_arready),		 // Templated
+		    .m1_rdata		(lsu_rdata[`AXI_DATA_BUS]), // Templated
+		    .m1_rresp		(lsu_rresp[`AXI_RESP_BUS]), // Templated
+		    .m1_rvalid		(lsu_rvalid),		 // Templated
+		    .m1_awready		(lsu_awready),		 // Templated
+		    .m1_wready		(lsu_wready),		 // Templated
+		    .m1_bresp		(lsu_bresp[`AXI_RESP_BUS]), // Templated
+		    .m1_bvalid		(lsu_bvalid),		 // Templated
+		    // Inputs
+		    .clk		(clk),			 // Templated
+		    .rst_n		(rst_n),		 // Templated
+		    .s0_arready		(sram_arready),		 // Templated
+		    .s0_rdata		(sram_rdata[`AXI_DATA_BUS]), // Templated
+		    .s0_rresp		(sram_rresp[`AXI_RESP_BUS]), // Templated
+		    .s0_rvalid		(sram_rvalid),		 // Templated
+		    .s0_awready		(sram_awready),		 // Templated
+		    .s0_wready		(sram_wready),		 // Templated
+		    .s0_bresp		(sram_bresp[`AXI_RESP_BUS]), // Templated
+		    .s0_bvalid		(sram_bvalid),		 // Templated
+		    .m0_araddr		(fetch_araddr),		 // Templated
+		    .m0_arvalid		(fetch_arvalid),	 // Templated
+		    .m0_rready		(fetch_rready),		 // Templated
+		    .m0_awaddr		(),			 // Templated
+		    .m0_awvalid		(),			 // Templated
+		    .m0_wdata		(),			 // Templated
+		    .m0_wstrb		(),			 // Templated
+		    .m0_wvalid		(),			 // Templated
+		    .m0_bready		(),			 // Templated
+		    .m1_araddr		(lsu_araddr),		 // Templated
+		    .m1_arvalid		(lsu_arvalid),		 // Templated
+		    .m1_rready		(lsu_rready),		 // Templated
+		    .m1_awaddr		(lsu_awaddr),		 // Templated
+		    .m1_awvalid		(lsu_awvalid),		 // Templated
+		    .m1_wdata		(lsu_wdata[`AXI_DATA_BUS]), // Templated
+		    .m1_wstrb		(lsu_wstrb),		 // Templated
+		    .m1_wvalid		(lsu_wvalid),		 // Templated
+		    .m1_bready		(lsu_bready));		 // Templated
 
 
-/*dsram AUTO_TEMPLATE (
+/*sram AUTO_TEMPLATE (
     .clk(clk),
     .rst_n(rst_n),
-    .araddr(lsu_araddr),
-    .arvalid(lsu_arvalid),
-    .arready(lsu_arready),
-    .rdata(lsu_rdata[`AXI_DATA_BUS]),
-    .rresp(lsu_rresp[`AXI_RESP_BUS]),
-    .rvalid(lsu_rvalid),
-    .rready(lsu_rready),
-    .awaddr(lsu_awaddr),
-    .awvalid(lsu_awvalid),
-    .awready(lsu_awready),
-    .wdata(lsu_wdata),
-    .wstrb(lsu_wstrb),
-    .wvalid(lsu_wvalid),
-    .wready(lsu_wready),
-    .bresp(lsu_bresp[`AXI_RESP_BUS]),
-    .bvalid(lsu_bvalid),
-    .bready(lsu_bready),
+    .araddr(sram_araddr),
+    .arvalid(sram_arvalid),
+    .arready(sram_arready),
+    .rdata(sram_rdata[`AXI_DATA_BUS]),
+    .rresp(sram_rresp[`AXI_RESP_BUS]),
+    .rvalid(sram_rvalid),
+    .rready(sram_rready),
+    .awaddr(sram_awaddr),
+    .awvalid(sram_awvalid),
+    .awready(sram_awready),
+    .wdata(sram_wdata),
+    .wstrb(sram_wstrb),
+    .wvalid(sram_wvalid),
+    .wready(sram_wready),
+    .bresp(sram_bresp[`AXI_RESP_BUS]),
+    .bvalid(sram_bvalid),
+    .bready(sram_bready),
 );
 */
-dsram dsram_u0 (/*AUTOINST*/
-		// Outputs
-		.arready		(lsu_arready),		 // Templated
-		.rdata			(lsu_rdata[`AXI_DATA_BUS]), // Templated
-		.rresp			(lsu_rresp[`AXI_RESP_BUS]), // Templated
-		.rvalid			(lsu_rvalid),		 // Templated
-		.awready		(lsu_awready),		 // Templated
-		.wready			(lsu_wready),		 // Templated
-		.bresp			(lsu_bresp[`AXI_RESP_BUS]), // Templated
-		.bvalid			(lsu_bvalid),		 // Templated
-		// Inputs
-		.clk			(clk),			 // Templated
-		.rst_n			(rst_n),		 // Templated
-		.araddr			(lsu_araddr),		 // Templated
-		.arvalid		(lsu_arvalid),		 // Templated
-		.rready			(lsu_rready),		 // Templated
-		.awaddr			(lsu_awaddr),		 // Templated
-		.awvalid		(lsu_awvalid),		 // Templated
-		.wdata			(lsu_wdata),		 // Templated
-		.wstrb			(lsu_wstrb),		 // Templated
-		.wvalid			(lsu_wvalid),		 // Templated
-		.bready			(lsu_bready));		 // Templated
+sram sram_u0 (/*AUTOINST*/
+	      // Outputs
+	      .arready			(sram_arready),		 // Templated
+	      .rdata			(sram_rdata[`AXI_DATA_BUS]), // Templated
+	      .rresp			(sram_rresp[`AXI_RESP_BUS]), // Templated
+	      .rvalid			(sram_rvalid),		 // Templated
+	      .awready			(sram_awready),		 // Templated
+	      .wready			(sram_wready),		 // Templated
+	      .bresp			(sram_bresp[`AXI_RESP_BUS]), // Templated
+	      .bvalid			(sram_bvalid),		 // Templated
+	      // Inputs
+	      .clk			(clk),			 // Templated
+	      .rst_n			(rst_n),		 // Templated
+	      .araddr			(sram_araddr),		 // Templated
+	      .arvalid			(sram_arvalid),		 // Templated
+	      .rready			(sram_rready),		 // Templated
+	      .awaddr			(sram_awaddr),		 // Templated
+	      .awvalid			(sram_awvalid),		 // Templated
+	      .wdata			(sram_wdata),		 // Templated
+	      .wstrb			(sram_wstrb),		 // Templated
+	      .wvalid			(sram_wvalid),		 // Templated
+	      .bready			(sram_bready));		 // Templated
 
 endmodule
 // Local Variables:
-// verilog-library-directories:("." "./core" "./perip")
+// verilog-library-directories:("." "./core" "./perip" "./bus")
 // verilog-library-extensions:(".v" ".sv")
 // verilog-auto-template-warn-unused: t
 // verilog-auto-tieoff-declaration: "assign"
