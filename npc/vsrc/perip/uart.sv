cc93187570612e5cc7769f900bc098ba5dba7a51
tracer-ysyx <tracer@ysyx.org>
2024-09-18 21:23:43 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:43 up  3:13,  1 user,  load average: 0.35, 0.12, 0.03

---
+++ npc/vsrc/perip/uart.sv
@@ -0,0 +1,33 @@
+`include "../inc/defines.svh"
+
+module uart (
+    input logic clk,
+    input logic rst_n,
+
+    // axi-lite interface (slave)
+    // AR
+    input logic [`AXI_ADDR_BUS] araddr,
+    input logic arvalid,
+    output logic arready,
+    // R
+    output logic [`AXI_DATA_BUS] rdata,
+    output logic [`AXI_RESP_BUS] rresp,
+    output logic rvalid,
+    input logic rready,
+    // AW
+    input logic [`AXI_ADDR_BUS] awaddr,
+    input logic awvalid,
+    output logic awready,
+    // W
+    input logic [`AXI_DATA_BUS] wdata,
+    input logic [`AXI_WSTRB_BUS] wstrb,
+    input logic wvalid,
+    output logic wready,
+    // B
+    output logic [`AXI_RESP_BUS] bresp,
+    output logic bvalid,
+    input logic bready
+);
+
+
+endmodule
