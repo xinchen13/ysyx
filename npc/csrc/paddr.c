989a622e1f92a6cf279fd950105279fe5028cda1
tracer-ysyx <tracer@ysyx.org>
2024-09-19 17:03:26 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:26 up 20:48,  1 user,  load average: 1.55, 1.09, 1.03

--- npc/csrc/paddr.c
+++ npc/csrc/paddr.c
@@ -54,12 +54,12 @@ int dpic_pmem_read(int raddr) {
         #endif 
         return read_data;
     }
-    // else if (aligned_address == 0xa0000048) {
-    //     struct timespec ts;
-    //     clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
-    //     int microseconds = ts.tv_nsec / 1000 + ts.tv_sec * 1000000;
-    //     return microseconds;
-    // }
+    else if (aligned_address == 0xa0000048) {
+        struct timespec ts;
+        clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
+        int microseconds = ts.tv_nsec / 1000 + ts.tv_sec * 1000000;
+        return microseconds;
+    }
     else {
         return -1;
     }
@@ -84,9 +84,9 @@ void dpic_pmem_write(int waddr, int wdata, char wmask) {
     // else if (waddr == 0xa00003f8) {
     //     putchar(wdata);
     // }
-    // else {
-    //     Assert(0, "wrong write: " FMT_PADDR, waddr);
-    // }
+    else {
+        Assert(0, "wrong write: " FMT_PADDR, waddr);
+    }
 }
 
 void uart_out(int wdata) {
