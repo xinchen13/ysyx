748da617d5dd2bfd056c650a16673d5b1d573552
tracer-ysyx <tracer@ysyx.org>
2024-09-19 22:41:52 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:41:52 up  4:39,  1 user,  load average: 0.39, 0.31, 0.16

--- npc/vsrc/perip/clint.sv
+++ npc/vsrc/perip/clint.sv
@@ -39,27 +39,106 @@ module clint (
         end
     end
 
-    assign arready = rready;
-    assign rvalid = arvalid;
+    logic [2:0] lfsr;
 
-    // read
+    localparam [1:0] IDLE = 2'b00;
+    localparam [1:0] READ = 2'b01;
+
+    logic [1:0] state;
+    logic [1:0] next_state;
+
+    // sram
+    logic sram_ack;                     // SRAM读取完成信号
+    logic [2:0] sram_wait_counter;      // 用于模拟不确定延迟的计数器
+
+    // state reg
+    always @ (posedge clk) begin
+        if (!rst_n) begin
+            state <= IDLE;
+        end 
+        else begin
+            state <= next_state;
+        end
+    end
+
+
+    assign arready = (state == IDLE) ? 1'b1 : 1'b0;
+    assign rresp = 2'b00;
+    assign rvalid = ((state == READ) && sram_ack) ? 1'b1 : 1'b0;
+
+    // trans logic
     always @ (*) begin
-        if (arvalid) begin
-            case (araddr)
-                32'ha0000048: begin
-                    rdata = mtime[31:0];
+        next_state = state;
+        case (state)
+            IDLE: begin
+                if (arvalid && arready) begin
+                    next_state = READ;  // 转移到READ状态
                 end
-                32'ha000004c: begin
-                    rdata = mtime[63:32];
+            end
+            READ: begin
+                if (rvalid && rready) begin
+                    next_state = IDLE;  // 数据传输完成，回到IDLE状态
+                end
+            end
+            default: begin
+                next_state = IDLE;
+            end
+        endcase
+    end
+
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            sram_ack  <= 1'b0;
+            sram_wait_counter <= 3'b000;  // 初始化等待计数器
+            rdata <= `INST_NOP;
+            lfsr <= 3'b001;
+        end 
+        else begin
+            case (state)
+                IDLE: begin
+                    sram_wait_counter <= 3'b000; // 重置等待计数器
+                    sram_ack <= 1'b0;
+                    lfsr <= {lfsr[1:0], lfsr[2] ^ lfsr[1]}; // LFSR反馈多项式, 伪随机延迟
+                end
+                READ: begin
+                    if (sram_wait_counter == lfsr) begin  // 模拟读取延迟
+                        rdata <= mtime[31:0];  // 从SRAM读取数据
+                        sram_ack   <= 1'b1;  // 读取完成信号
+                        sram_wait_counter <= 3'b000; // 重置等待计数器
+                    end 
+                    else begin
+                        sram_ack <= 1'b0;
+                        sram_wait_counter <= sram_wait_counter + 1;
+                    end
                 end
                 default: begin
-                    rdata = 'b0;
                 end
             endcase
         end
-        else begin
-            rdata = 'b0;
-        end
     end
+
+    // assign arready = rready;
+    // assign rvalid = arvalid;
+
+    // // read
+    // always @ (*) begin
+    //     if (arvalid) begin
+    //         case (araddr)
+    //             32'ha0000048: begin
+    //                 rdata = mtime[31:0];
+    //             end
+    //             32'ha000004c: begin
+    //                 rdata = mtime[63:32];
+    //             end
+    //             default: begin
+    //                 rdata = 'b0;
+    //             end
+    //         endcase
+    //     end
+    //     else begin
+    //         rdata = 'b0;
+    //     end
+    // end
 
 endmodule
