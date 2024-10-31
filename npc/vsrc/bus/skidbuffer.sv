19ae36ef9142e0139dee6cfe374ec6b4721fee38
tracer-ysyx <tracer@ysyx.org>
2024-10-31 17:43:24 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:43:24 up 1 day,  7:01,  1 user,  load average: 0.50, 0.23, 0.20

--- npc/vsrc/bus/skidbuffer.sv
+++ npc/vsrc/bus/skidbuffer.sv
@@ -12,8 +12,6 @@ module skidbuffer #(
 	output	reg		[DW-1:0]	o_data
 );
 
-	wire	[DW-1:0]	w_data;
-
 	// real skid buffer
 	reg					r_valid;
 	reg		[DW-1:0]	r_data;
@@ -23,40 +21,38 @@ module skidbuffer #(
 		if (i_reset)
 			r_valid <= 0;
 		else if ((i_valid && o_ready) && (o_valid && !i_ready))
-			// We have incoming data, but the output is stalled
-			r_valid <= 1;
+			// data's incoming, but the output is stalled
+			r_valid <= 1; 
 		else if (i_ready)
 			r_valid <= 0;
 	end
 
 	// r_data
-	always @ (posedge i_clk)
-	if (i_reset)
-		r_data <= 0;
-	else if (OPT_LOWPOWER && (!o_valid || i_ready))
-		r_data <= 0;
-	else if ((!OPT_LOWPOWER || !OPT_OUTREG || i_valid) && o_ready)
-		r_data <= i_data;
-
-	assign	w_data = r_data;
+	always @ (posedge i_clk) begin
+		if (i_reset)
+			r_data <= 0;
+		else if (OPT_LOWPOWER && (!o_valid || i_ready))
+			r_data <= 0;
+		else if ((!OPT_LOWPOWER || !OPT_OUTREG || i_valid) && o_ready)
+			r_data <= i_data;
+	end
 
 	// o_ready
 	assign o_ready = !r_valid;
-
-	// And then move on to the output port
 
 	// Outputs are combinatorially determined from inputs
 	// o_valid
 	assign	o_valid = !i_reset && (i_valid || r_valid);
 
 	// o_data
-	always @(*)
-	if (r_valid)
-		o_data = r_data;
-	else if (!OPT_LOWPOWER || i_valid)
-		o_data = i_data;
-	else
-		o_data = 0;
+	always @(*) begin
+		if (r_valid)
+			o_data = r_data;
+		else if (!OPT_LOWPOWER || i_valid)
+			o_data = i_data;
+		else
+			o_data = 0;
+	end
 
 
 endmodule
