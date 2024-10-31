311983c92d62265b831b8e2b719a4bf016fadd58
tracer-ysyx <tracer@ysyx.org>
2024-10-31 15:09:45 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:09:45 up 1 day,  4:27,  1 user,  load average: 0.51, 0.34, 0.37

--- npc/vsrc/bus/addrdecode.sv
+++ npc/vsrc/bus/addrdecode.sv
@@ -1,10 +1,6 @@
-module	addrdecode #(
-		// {{{
-		parameter	NS=8,
+module addrdecode #(
+		parameter	NS = 8,
 		parameter	AW = 32, DW=32+32/8+1+1,
-		//
-		// SLAVE_ADDR contains address assignments for each of the
-		// various slaves we are adjudicating between.
 		parameter	[NS*AW-1:0]	SLAVE_ADDR = {
 				{ 3'b111,  {(AW-3){1'b0}} },
 				{ 3'b110,  {(AW-3){1'b0}} },
@@ -14,10 +10,6 @@ module	addrdecode #(
 				{ 3'b010,  {(AW-3){1'b0}} },
 				{ 4'b0010, {(AW-4){1'b0}} },
 				{ 4'b0000, {(AW-4){1'b0}} }},
-		//
-		// SLAVE_MASK contains a mask of those address bits in
-		// SLAVE_ADDR which are relevant.  It shall be true that if
-		// !SLAVE_MASK[k] then !SLAVE_ADDR[k], for any bits of k
 		parameter	[NS*AW-1:0]	SLAVE_MASK = (NS <= 1) ? 0
 			: { {(NS-2){ 3'b111, {(AW-3){1'b0}} }},
 				{(2){ 4'b1111, {(AW-4){1'b0}} }} },
@@ -35,13 +27,7 @@ module	addrdecode #(
 		// If OPT_REGISTERED is set, address decoding will take an extra
 		// clock, and will register the results of the decoding
 		// operation.
-		parameter [0:0]	OPT_REGISTERED = 0,
-		//
-		// If OPT_LOWPOWER is set, then whenever the output is not
-		// valid, any respective data linse will also be forced to zero
-		// in an effort to minimize power.
-		parameter [0:0]	OPT_LOWPOWER = 0
-		// }}}
+		parameter [0:0]	OPT_REGISTERED = 0
 	) (
 		// {{{
 		input	wire			i_clk, i_reset,
@@ -174,16 +160,16 @@ module	addrdecode #(
 		initial	o_addr   = 0;
 		initial	o_data   = 0;
 		always @(posedge i_clk)
-		if (i_reset && OPT_LOWPOWER)
+		if (i_reset)
 		begin
 			o_addr   <= 0;
 			o_data   <= 0;
 		end else if ((!o_valid || !i_stall)
-				 && (i_valid || !OPT_LOWPOWER))
+				 && (i_valid))
 		begin
 			o_addr   <= i_addr;
 			o_data   <= i_data;
-		end else if (OPT_LOWPOWER && !i_stall)
+		end else if (!i_stall)
 		begin
 			o_addr   <= 0;
 			o_data   <= 0;
@@ -197,9 +183,9 @@ module	addrdecode #(
 		if (i_reset)
 			o_decode <= 0;
 		else if ((!o_valid || !i_stall)
-				 && (i_valid || !OPT_LOWPOWER))
+				 && (i_valid))
 			o_decode <= request;
-		else if (OPT_LOWPOWER && !i_stall)
+		else if (!i_stall)
 			o_decode <= 0;
 		// }}}
 
@@ -221,172 +207,8 @@ module	addrdecode #(
 		end
 
 		// Make Verilator happy
-		// {{{
-		// verilator lint_off UNUSED
-		wire	unused;
-		assign	unused = &{ 1'b0,
-`ifdef	VERILATOR
-				// Can't declare the clock as unused for formal,
-				// lest it not be recognized as *the* clock
-				i_clk,
-`endif
-				i_reset };
-		// verilator lint_on UNUSED
-		// }}}
-	end endgenerate
-	// }}}
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-//
-// Formal properties
-// {{{
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-`ifdef	FORMAL
-	reg	f_past_valid;
-	initial	f_past_valid = 0;
-	always @(posedge i_clk)
-		f_past_valid <= 1;
-
-	reg	[AW+DW-1:0]	f_idata;
-	always @(*)
-		f_idata = { i_addr, i_data };
-
-`ifdef	ADDRDECODE
-	always @(posedge i_clk)
-	if (!f_past_valid)
-		assume(i_reset);
-`else
-	always @(posedge i_clk)
-	if (!f_past_valid)
-		assert(i_reset);
-
-`endif	// ADDRDECODE
-	always @(posedge i_clk)
-	if (OPT_REGISTERED && (!f_past_valid || $past(i_reset)))
-	begin
-		assert(!o_valid);
-		assert(o_decode == 0);
-	end else if ($past(o_valid && i_stall) && OPT_REGISTERED)
-	begin
-		assert($stable(o_addr));
-		assert($stable(o_decode));
-		assert($stable(o_data));
-	end
-
-	// If the output is ever valid, there must be at least one
-	// decoded output
-	always @(*)
-		assert(o_valid == (o_decode != 0));
-
-	always @(*)
-	for(iM=0; iM<NS; iM=iM+1)
-	if (o_decode[iM])
-	begin
-		// The address must match
-		assert((((o_addr ^ SLAVE_ADDR[iM*AW +: AW])
-				& SLAVE_MASK[iM*AW +: AW])==0)
-			&& ACCESS_ALLOWED[iM]);
-		//
-		// And nothing else must match
-		assert(o_decode == (1<<iM));
-	end
-
-	always @(*)
-	for(iM=0; iM<NS; iM=iM+1)
-	if (!ACCESS_ALLOWED[iM])
-		assert(!o_decode[iM]);
-
-	// LOWPOWER check
-	// {{{
-	generate if (OPT_LOWPOWER && OPT_REGISTERED)
-	begin
-		always @(*)
-		if (!o_valid)
-		begin
-			assert(o_addr   == 0);
-			assert(o_decode == 0);
-			assert(o_data   == 0);
-		end
-	end endgenerate
-	// }}}
-
-	//
-	// The output decoded value may only ever have one value high,
-	// never more--i.e. $onehot0
-	// {{{
-`ifdef	VERIFIC
-	always @(*)
-		assert($onehot0(request));
-`else
-	reg	onehot_request;
-	always @(*)
-	begin
-		onehot_request = 0;
-		for(iM=0; iM<NS+1; iM=iM+1)
-		if ((request ^ (1<<iM))==0)
-			onehot_request = 1;
-	end
-
-	always @(*)
-	if (request != 0)
-		assert(onehot_request);
-`endif
-	// }}}
-
-	////////////////////////////////////////////////////////////////////////
-	//
-	// Cover properties
-	// {{{
-	////////////////////////////////////////////////////////////////////////
-	//
-	//
-
-	//
-	// Make sure all addresses are reachable
-	//
-	reg	[NS:0]	f_reached;
-
-	always @(posedge i_clk)
-		cover(i_valid);
-
-	always @(posedge i_clk)
-		cover(o_valid);
-
-	always @(posedge i_clk)
-		cover(o_valid && !i_stall);
-
-	initial	f_reached = 0;
-	always @(posedge i_clk)
-	if (i_reset)
-		f_reached = 0;
-	else if (o_valid)
-		f_reached = f_reached | o_decode;
-
-	generate if (!OPT_NONESEL && ACCESS_ALLOWED[0]
-			&& SLAVE_MASK == 0 && NS == 1)
-	begin
-
-		always @(*)
-			cover(f_reached[0]);
-
-		always @(posedge i_clk)
-		if (f_past_valid && $stable(o_valid))
-			assert($stable(o_decode));
-
-	end else begin
-
-		always @(*)
-			cover(&f_reached);
-
-		always @(posedge i_clk)
-		if (f_past_valid && $stable(o_valid))
-			cover($changed(o_decode));
+		// wire	unused;
+		// assign	unused = &{ 1'b0, i_reset };
 
 	end endgenerate
-	// }}}
-`endif	// FORMAL
-// }}}
 endmodule
