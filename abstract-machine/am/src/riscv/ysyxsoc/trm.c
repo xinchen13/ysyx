074279ad44fe5239d1978fc343356d74dc9fe764
tracer-ysyx <tracer@ysyx.org>
2024-11-13 21:26:40 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:40 up 55 min,  0 users,  load average: 0.40, 0.14, 0.04

---
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -0,0 +1,36 @@
+#include <am.h>
+#include <klib-macros.h>
+
+extern char _heap_start;
+int main(const char *args);
+
+#define HEAP_SIZE 0x1fff
+#define HEAP_END  ((uintptr_t)&_heap_start + HEAP_SIZE)
+
+Area heap = RANGE(&_heap_start, HEAP_END);
+#ifndef MAINARGS
+#define MAINARGS ""
+#endif
+static const char mainargs[] = MAINARGS;
+
+// ------- simulate serial port -------
+#define UART_BASE       0x10000000
+#define TX_REG          (UART_BASE + 0x0)
+static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
+// ------------------------------------
+
+void putch(char ch) {
+    outb(TX_REG, ch);
+}
+
+#define ysyxsoc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+
+void halt(int code) {
+    ysyxsoc_trap(code);
+    while(1);       // should not reach here
+}
+
+void _trm_init() {
+    int ret = main(mainargs);
+    halt(ret);
+}
