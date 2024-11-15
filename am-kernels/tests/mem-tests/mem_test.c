ef4b2dd6fb0dd26c720eaa686413897fc059c757
tracer-ysyx <tracer@ysyx.org>
2024-11-15 14:18:08 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:18:08 up  4:53,  0 users,  load average: 0.26, 0.15, 0.06

--- am-kernels/tests/mem-tests/mem_test.c
+++ am-kernels/tests/mem-tests/mem_test.c
@@ -4,9 +4,6 @@
 #define SRAM_END    0x0f001000
 
 int main () {
-    uint8_t cnt_8_lo = 0;
-    // uint8_t cnt_8_hi = 0;
-    uint16_t cnt_8 = 0;
     volatile uint8_t *write_ptr_8 = (uint8_t *)SRAM_START;
     volatile uint8_t *check_ptr_8 = (uint8_t *)SRAM_START;
 
@@ -19,19 +16,14 @@ int main () {
     // check
     while ((uint32_t)check_ptr_8 < SRAM_END) {
         if (*check_ptr_8 == (uint8_t)((uint32_t)check_ptr_8)) {
-            cnt_8++;
+            ;
         } else {
             // putch('F');
             halt(1);
         }
         check_ptr_8++;
     }
-    
-    // cnt_8_hi = (uint8_t)(cnt_8 >> 8);
-    // putch(cnt_8_hi);
-
-    cnt_8_lo = (uint8_t)cnt_8;
-    putch(cnt_8_lo);
+    putch((char)(*check_ptr_8));
 
     return 0;
 }
