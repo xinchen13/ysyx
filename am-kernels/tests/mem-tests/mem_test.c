d384faf0419618a25de161d01163d66a7734af8f
tracer-ysyx <tracer@ysyx.org>
2024-11-15 14:07:01 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:07:01 up  4:41,  0 users,  load average: 0.18, 0.10, 0.02

--- am-kernels/tests/mem-tests/mem_test.c
+++ am-kernels/tests/mem-tests/mem_test.c
@@ -16,9 +16,11 @@ int main () {
     // check
     while ((uint32_t)check_ptr_8 < SRAM_END) {
         if (*check_ptr_8 == (uint8_t)((uint32_t)check_ptr_8)) {
-            putch('P');
+            // putch('P');
+            ;
         } else {
-            putch('F');
+            // putch('F');
+            halt(1);
         }
         check_ptr_8++;
     }
