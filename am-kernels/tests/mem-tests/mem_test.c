9c97f738ad6fb30537c393a9de68ed9256d63b7d
tracer-ysyx <tracer@ysyx.org>
2024-11-15 14:29:58 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:29:58 up  5:04,  1 user,  load average: 0.04, 0.16, 0.10

--- am-kernels/tests/mem-tests/mem_test.c
+++ am-kernels/tests/mem-tests/mem_test.c
@@ -6,13 +6,11 @@
 int main () {
     volatile uint8_t *write_ptr_8 = (uint8_t *)SRAM_START;
     volatile uint8_t *check_ptr_8 = (uint8_t *)SRAM_START;
-
     // write 
     while ((uint32_t)write_ptr_8 < SRAM_END) {
         *write_ptr_8 = (uint32_t)write_ptr_8;
         write_ptr_8++;
     }
-
     // check
     while ((uint32_t)check_ptr_8 < SRAM_END) {
         if (*check_ptr_8 == (uint8_t)((uint32_t)check_ptr_8)) {
@@ -26,16 +24,14 @@ int main () {
 
     volatile uint16_t *write_ptr_16 = (uint16_t *)SRAM_START;
     volatile uint16_t *check_ptr_16 = (uint16_t *)SRAM_START;
-
     // write 
     while ((uint32_t)write_ptr_16 < SRAM_END) {
         *write_ptr_16 = (uint32_t)write_ptr_16;
         write_ptr_16++;
     }
-
     // check
     while ((uint32_t)check_ptr_16 < SRAM_END) {
-        if (*check_ptr_16 == (uint8_t)((uint32_t)check_ptr_16)) {
+        if (*check_ptr_16 == (uint16_t)((uint32_t)check_ptr_16)) {
             ;
         } else {
             // putch('F');
