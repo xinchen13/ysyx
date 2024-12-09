46b2a1b9451cbaa993217b1d6a745763272122cf
xinchen <xinchen.sakura@gmail.com>
2024-12-09 23:48:48 +0800

    update

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -16,15 +16,26 @@ void bootloader() {
 // *******************************************
 
 // ****************** uart *******************
-#define UART_BASE       0x10000000
+#define UART_BASE       0x10000000L
 #define TX_REG          (UART_BASE + 0x0)
+#define LCR             (UART_BASE + 0x3)
+#define LSR             (UART_BASE + 0x5)
+#define DLL             (UART_BASE + 0x0)
+#define DLH             (UART_BASE + 0x1)
+static inline uint8_t inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
 static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
 
 void uart_init() {
-
+    outb(LCR, 0x80);        // DLAB = 1
+    outb(DLL, 0x01);
+    outb(DLH, 0x00);
+    outb(LCR, 0x03);        // reset value
 }
 
 void putch(char ch) {
+    // while((inb(LSR) & 0x20) == 0x0){
+    //     ;
+    // }
     outb(TX_REG, ch);
 }
 // *******************************************
