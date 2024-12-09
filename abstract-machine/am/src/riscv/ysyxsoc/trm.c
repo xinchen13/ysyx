cc16d8c77ad87cbbd674714f39959b08a125a19c
tracer-ysyx <tracer@ysyx.org>
2024-12-09 23:13:58 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:13:58 up  1:00,  2 users,  load average: 0.17, 0.14, 0.13

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -19,8 +19,10 @@ void bootloader() {
 #define UART_BASE       0x10000000L
 #define TX_REG          (UART_BASE + 0x0)
 #define LCR             (UART_BASE + 0x3)
+#define LSR             (UART_BASE + 0x5)
 #define DLL             (UART_BASE + 0x0)
 #define DLH             (UART_BASE + 0x1)
+static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
 static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
 
 void uart_init() {
@@ -31,6 +33,7 @@ void uart_init() {
 }
 
 void putch(char ch) {
+    while(!(inb(LSR) & 0x20));
     outb(TX_REG, ch);
 }
 // *******************************************
