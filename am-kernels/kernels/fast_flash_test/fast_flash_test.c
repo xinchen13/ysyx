80feead6fd31c82ff5398a954e233d81991780a6
tracer-ysyx <tracer@ysyx.org>
2024-12-12 18:08:18 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:08:18 up  8:23,  3 users,  load average: 0.24, 0.29, 0.25

--- am-kernels/kernels/fast_flash_test/fast_flash_test.c
+++ am-kernels/kernels/fast_flash_test/fast_flash_test.c
@@ -4,16 +4,21 @@
 static inline uint8_t inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
 
 int main(const char *args) {
-  // const char *fmt =
-  //   "Hello, AbstractMachine!\n"
-  //   "mainargs = '%'.\n";
+    const char *fmt = "fast flash test!\n";
+    for (const char *p = fmt; *p; p++) {
+        putch(*p);
+    }
 
-  // for (const char *p = fmt; *p; p++) {
-  //   (*p == '%') ? putstr(args) : putch(*p);
-  // }
-  uint8_t init_data = inb(0x30000004);
-  putch('\n');
-  putch(init_data);
+  uint8_t init_data[4];
+  init_data[0] = inb(0x30000004);
+  init_data[1] = inb(0x30000005);
+  init_data[2] = inb(0x30000006);
+  init_data[3] = inb(0x30000007);
+
+  putch(init_data[0]);
+  putch(init_data[1]);
+  putch(init_data[2]);
+  putch(init_data[3]);
   putch('\n');
   
   return 0;
