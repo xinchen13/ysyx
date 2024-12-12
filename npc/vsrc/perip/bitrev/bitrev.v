3c74edc7c789c6d55674085155aba70075117a6e
xinchen <xinchen.sakura@gmail.com>
2024-12-12 23:48:20 +0800

    update spi

--- npc/vsrc/perip/bitrev/bitrev.v
+++ npc/vsrc/perip/bitrev/bitrev.v
@@ -1,8 +1,31 @@
 module bitrev (
-  input  sck,
-  input  ss,
-  input  mosi,
-  output miso
+    input  sck,
+    input  ss,
+    input  mosi,
+    output miso
 );
-  assign miso = 1'b1;
+    // assign miso = 1'b1;
+
+    reg [7:0] data_reg;
+
+    // write
+    always @ (posedge sck) begin
+        if (!ss) begin
+            data_reg <= {data_reg[6:0], mosi};
+        end
+        else begin
+            data_reg <= 'b0;
+        end
+    end
+
+    // read
+    always @ (posedge sck) begin
+        if (!ss) begin
+            miso <= {data_reg[6:0], mosi};
+        end
+        else begin
+            miso <= 'b1;
+        end
+    end
+
 endmodule
