7d617da5bcaf5ebfff16dae3b0fde5a43c49e375
tracer-ysyx <tracer@ysyx.org>
2024-12-12 15:06:21 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:21 up  5:21,  3 users,  load average: 0.27, 0.18, 0.20

--- npc/csrc/fast_flash.c
+++ npc/csrc/fast_flash.c
@@ -3,18 +3,18 @@
 static uint8_t flash_mem[FLASH_MSIZE] = {};
 
 void init_fast_flash() {
-    // uint8_t* addr_ptr = flash_mem;
-    // while (addr_ptr < flash_mem + FLASH_MSIZE) {
-    //     *addr_ptr = 0xff;
-    //     addr_ptr++;
-    // }
-    memset(flash_mem, rand(), FLASH_MSIZE);
+    uint8_t* addr_ptr = flash_mem;
+    while (addr_ptr < flash_mem + FLASH_MSIZE) {
+        *addr_ptr = 0xff;
+        addr_ptr++;
+    }
+    // memset(flash_mem, rand(), FLASH_MSIZE);
     Log("flash(simulation) memory area [" FMT_PADDR ", " FMT_PADDR "]", FLASH_LEFT, FLASH_RIGHT);
 }
 
 void flash_read(int32_t addr, int32_t *data) { 
-    // assert(0); 
-    uint8_t* flash_addr = flash_mem + addr - FLASH_BASE;
-    *data = *(uint8_t  *)flash_addr;
+    assert(0); 
+    // uint8_t* flash_addr = flash_mem + addr - FLASH_BASE;
+    // *data = *(uint8_t  *)flash_addr;
 }
 
