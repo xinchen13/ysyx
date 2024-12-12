8adb9dc1a583e5e6569e9a01f7b66df488abfa4c
tracer-ysyx <tracer@ysyx.org>
2024-12-12 17:45:07 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:45:07 up  7:59,  3 users,  load average: 1.62, 0.60, 0.32

--- npc/csrc/fast_flash.c
+++ npc/csrc/fast_flash.c
@@ -4,9 +4,11 @@ static uint8_t flash_mem[FLASH_MSIZE];
 
 void init_fast_flash() {
     uint8_t* addr_ptr = flash_mem;
+    uint8_t  wdata = 0x30;
     while (addr_ptr < flash_mem + FLASH_MSIZE) {
-        *addr_ptr = 0x33;
+        *addr_ptr = wdata;
         addr_ptr++;
+        wdata = (wdata == 0x39) ? 0x30 : wdata++;
     }
     // memset(flash_mem, rand(), FLASH_MSIZE);
     Log("flash(simulation) memory area [" FMT_PADDR ", " FMT_PADDR "]", FLASH_LEFT, FLASH_RIGHT);
