1a9abd9115b35025834484fe63741969b49fda2f
tracer-ysyx <tracer@ysyx.org>
2024-12-15 12:42:26 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:42:26 up 25 min,  2 users,  load average: 0.27, 0.26, 0.21

--- am-kernels/kernels/spi_bitrev/spi_bitrev.c
+++ am-kernels/kernels/spi_bitrev/spi_bitrev.c
@@ -20,7 +20,7 @@ static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)a
 
 int main(const char *args) {
     // tx data
-    outb(SPI_TX_REG0, 0x41);
+    outb(SPI_TX_REG0, 0b11001110);        // s: 0x73, bitrev: 0b11001110
 
     // divider
     outw(SPI_DIVIDER, 0xffff);
