6cadcaa8660a3484d263b8b720aac527a50ed595
tracer-ysyx <tracer@ysyx.org>
2024-12-15 13:06:38 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:06:38 up 50 min,  3 users,  load average: 0.61, 0.61, 0.48

--- npc/vsrc/perip/bitrev/bitrev.v
+++ npc/vsrc/perip/bitrev/bitrev.v
@@ -33,7 +33,7 @@ module bitrev (
                     rx_reg <= {rx_reg[6:0], mosi};
                     if (bit_cnt == 3'd7) begin
                         state <= TX;
-                        miso <= rx_reg[bit_cnt];
+                        miso <= mosi;
                         bit_cnt <= bit_cnt - 1'b1;
                     end
                     else begin
