f0a635a9b8662c2f3e3ad83b609a10221634fe10
tracer-ysyx <tracer@ysyx.org>
2024-12-23 23:53:59 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:53:59 up  2:58,  3 users,  load average: 0.17, 0.14, 0.15

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -36,7 +36,7 @@ __attribute__((noinline)) void uart_init() {
 
 void putch(char ch) {
     // polling
-    while((inb(LSR) & 0x20) == 0x20){
+    while((inb(LSR) & 0x40) == 0x40){
         ;
     }
     outb(TX_REG, ch);
