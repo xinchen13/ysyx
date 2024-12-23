cac58f1680553ddf00980693d988a3bcae05f4ae
tracer-ysyx <tracer@ysyx.org>
2024-12-23 21:46:57 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:46:56 up 50 min,  2 users,  load average: 0.24, 0.27, 0.22

--- am-kernels/kernels/spi_flash_test/spi_flash_test.c
+++ am-kernels/kernels/spi_flash_test/spi_flash_test.c
@@ -40,14 +40,24 @@ uint32_t flash_read(uint32_t addr) {
 }
 
 int main(const char *args) {
-    uint32_t init_data;
-    putstr("Start - reading falsh\n");
-    init_data = flash_read(0x30000010);
-    putstr("End - reading falsh\n");
-    putch(init_data);
-    putch(init_data>>8);
-    putch(init_data>>16);
-    putch(init_data>>24);
+    uint32_t init_data_spi;
+    putstr("Start - reading falsh (through spi)\n");
+    init_data_spi = flash_read(0x30000010);
+    putstr("End - reading falsh (through spi)\n");
+    putch(init_data_spi);
+    putch(init_data_spi>>8);
+    putch(init_data_spi>>16);
+    putch(init_data_spi>>24);
+    putch('\n');
+
+    uint32_t init_data_xip;
+    putstr("Start - reading falsh (XIP)\n");
+    init_data_xip = flash_read(0x30000010);
+    putstr("End - reading falsh (XIP)\n");
+    putch(init_data_xip);
+    putch(init_data_xip>>8);
+    putch(init_data_xip>>16);
+    putch(init_data_xip>>24);
     putch('\n');
 
     return 0;
