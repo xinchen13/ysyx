9018d44f781837b5a5cc2908df12b283191fc24b
tracer-ysyx <tracer@ysyx.org>
2024-12-24 14:50:38 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:50:38 up 15:29,  4 users,  load average: 0.18, 0.27, 0.34

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -10,7 +10,8 @@ AM_SRCS := riscv/ysyxsoc/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/ysyxsoc_linker.ld \
-						 --defsym=_pmem_start=0x30000000 --defsym=_entry_offset=0x0
+						#  --defsym=_pmem_start=0x20000000 \
+                         --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
