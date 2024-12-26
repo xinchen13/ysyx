4a4c176d5e0d99100d071d7241cd838a1117f6b3
xinchen <xinchen.sakura@gmail.com>
2024-12-26 17:00:23 +0800

    update psram model

--- npc/vsrc/perip/psram/psram.v
+++ npc/vsrc/perip/psram/psram.v
@@ -11,12 +11,33 @@ module psram(
 
     wire reset = ce_n;
 
-    typedef enum [2:0] { cmd_t, addr_t, data_t, err_t } state_t;
+    typedef enum [2:0] { cmd_t, addr_t, wait_t, data_t, err_t } state_t;
     reg [2:0]  state;
     reg [7:0]  counter;
     reg [7:0]  cmd;
     reg [23:0] addr;
     reg [31:0] data;
+
+    always@(posedge sck or posedge reset) begin
+        if (reset) begin
+            state <= cmd_t;
+        end
+        else begin
+            case (state)
+                cmd_t:  state <= (counter == 8'd7 ) ? addr_t : state;
+                addr_t: state <= ((cmd != 8'hEB) && (cmd != 8'h38) ) ? err_t  :
+                                (counter == 8'd5) ? wait_t : state;
+                wait_t: state <= (counter == 8'd5) ? data_t : state;
+                data_t: state <= state;
+
+                default: begin
+                    state <= state;
+                    $write("Assertion failed: Unsupported command `%xh`, only support `03h` read command\n", cmd);
+                    $fatal;
+                end
+            endcase
+        end
+    end
 
 
 
