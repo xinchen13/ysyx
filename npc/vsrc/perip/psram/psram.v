56f4aaacb268c8ced99f921e5f39e30d7d59dd56
tracer-ysyx <tracer@ysyx.org>
2024-12-26 15:07:02 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:07:02 up  5:37,  2 users,  load average: 0.86, 0.51, 0.34

--- npc/vsrc/perip/psram/psram.v
+++ npc/vsrc/perip/psram/psram.v
@@ -1,9 +1,23 @@
+// import "DPI-C" function int psram_read(input int addr);
+// import "DPI-C" function void psram_write(input int addr, input int data);
+
+// reference: ../perip/flash/flash.v
+
 module psram(
-  input sck,
-  input ce_n,
-  inout [3:0] dio
+    input sck,
+    input ce_n,
+    inout [3:0] dio
 );
 
-  assign dio = 4'bz;
+    wire reset = ce_n;
+
+    typedef enum [2:0] { cmd_t, addr_t, data_t, err_t } state_t;
+    reg [2:0]  state;
+    reg [7:0]  counter;
+    reg [7:0]  cmd;
+    reg [23:0] addr;
+    reg [31:0] data;
+
+
 
 endmodule
