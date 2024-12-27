e4ae325b8168c131bb655ba82af7be8cec543bab
tracer-ysyx <tracer@ysyx.org>
2024-12-27 13:29:58 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:29:58 up 13:57,  4 users,  load average: 0.58, 0.47, 0.42

--- npc/csrc/paddr.c
+++ npc/csrc/paddr.c
@@ -150,10 +150,10 @@ void flash_read(int32_t addr, int32_t *data) {
 
 int psram_read(int addr) {
     // raddr & ~0x3u
-    int aligned_address = (addr & (~0x3u)) + CONFIG_PSRAM_BASE;
+    uint32_t aligned_address = (addr & (~0x3u)) + CONFIG_PSRAM_BASE;
     Log(" read %d (bytes)  @addr = " FMT_WORD, 4, aligned_address);
     if (aligned_address >= CONFIG_PSRAM_BASE && aligned_address <= (CONFIG_PSRAM_BASE + CONFIG_PSRAM_SIZE)) {
-        int read_data = host_read(psram + aligned_address - CONFIG_PSRAM_BASE, 4);
+        int read_data = host_read((psram + aligned_address - CONFIG_PSRAM_BASE), 4);
         // memory trace
         #ifdef CONFIG_MTRACE
             Log(" read %d (bytes)  @addr = " FMT_WORD, 4, aligned_address);
