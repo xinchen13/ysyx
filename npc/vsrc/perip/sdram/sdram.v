75dbc82ec15871eee33d4099bad5b794b73bd510
xinchen <xinchen.sakura@gmail.com>
2025-01-03 00:03:57 +0800

    update sdram

--- npc/vsrc/perip/sdram/sdram.v
+++ npc/vsrc/perip/sdram/sdram.v
@@ -11,6 +11,17 @@ module sdram(
     inout [15:0] dq
 );
 
-    assign dq = 16'bz;
+    reg [12:0] mode_reg;
+
+    // cmd
+    localparam  NOP       =     3'b111;
+    localparam  ACTIVE    =     3'b011;
+    localparam  READ      =     3'b101;
+    localparam  WRITE     =     3'b100;
+    localparam  TERMINATE =     3'b110;
+    localparam  PRECHARGE =     3'b010;
+    localparam  REFRESH   =     3'b001;
+    localparam  MODE_REG  =     3'b000;
+
 
 endmodule
