d612a3fd067f96362ca0e6370a3ca3ff20ee0941
tracer-ysyx <tracer@ysyx.org>
2025-01-07 22:08:23 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:08:23 up  2:17,  1 user,  load average: 0.57, 0.34, 0.26

--- npc/vsrc/perip/ps2/ps2_top_apb.v
+++ npc/vsrc/perip/ps2/ps2_top_apb.v
@@ -1,19 +1,72 @@
 module ps2_top_apb(
-  input         clock,
-  input         reset,
-  input  [31:0] in_paddr,
-  input         in_psel,
-  input         in_penable,
-  input  [2:0]  in_pprot,
-  input         in_pwrite,
-  input  [31:0] in_pwdata,
-  input  [3:0]  in_pstrb,
-  output        in_pready,
-  output [31:0] in_prdata,
-  output        in_pslverr,
+    input         clock,
+    input         reset,
+    input  [31:0] in_paddr,
+    input         in_psel,
+    input         in_penable,
+    input  [2:0]  in_pprot,
+    input         in_pwrite,
+    input  [31:0] in_pwdata,
+    input  [3:0]  in_pstrb,
+    output reg         in_pready,
+    output reg  [31:0] in_prdata,
+    output reg         in_pslverr,
 
-  input         ps2_clk,
-  input         ps2_data
+    input         ps2_clk,
+    input         ps2_data
 );
+    wire ready;
+    wire overflow;
+    wire [7:0] scan_code;
+    reg nextdata_n;
+    wire nextdata_n_wire = nextdata_n;
+
+    // state machine
+    reg state;
+    localparam IDLE     = 1'b0;
+    localparam READ     = 1'b1;
+
+    always @ (posedge clock) begin
+        if (reset) begin
+            state       <= IDLE;
+            in_prdata   <= 'b0;
+            in_pready   <= 'b0;
+            in_pslverr  <= 'b0;
+            nextdata_n  <= 'b1;
+        end
+        else begin
+            case (state)
+                IDLE: begin
+                    in_prdata   <= 'b0;
+                    in_pready   <= 'b0;
+                    in_pslverr  <= 'b0;
+                    nextdata_n  <= 'b1;
+                    if (in_psel & !in_penable) begin
+                        state <= READ;
+                    end
+                end
+                READ: begin
+                    if (in_psel & in_penable) begin
+                        state       <= IDLE;
+                        nextdata_n  <= 'b0;
+                        in_prdata   <= ready ? {24'b0, scan_code} : 'b0;
+                        in_pready   <= 'b1;
+                        in_pslverr  <= 'b0;
+                    end
+                end
+            endcase
+        end
+    end
+
+    ps2_keyboard u_ps2_keyboard (
+        .clk(clock),
+        .rst_n(~reset),
+        .ps2_clk(ps2_clk),
+        .ps2_data(ps2_data),
+        .nextdata_n(nextdata_n_wire),
+        .scan_code(scan_code),
+        .ready(ready),
+        .overflow(overflow)
+    );
 
 endmodule
