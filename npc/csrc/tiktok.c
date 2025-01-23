59249a4147f6f5f572593ec54d1f967e848c7e0e
xinchen <xinchen.sakura@gmail.com>
2025-01-24 01:04:53 +0800

    update

--- npc/csrc/tiktok.c
+++ npc/csrc/tiktok.c
@@ -34,16 +34,16 @@ static void pmu_display() {
 
     // log out
     Log("********** Performance Monitor **********");
-    Log("Total cycle count = %" PRIu64 "", cycle_count);
-    Log("Total insts count = %" PRIu64 "", inst_count);
-    Log("   - A(alu) type count = %" PRIu64 "", a_type);
-    Log("   - B(branch) type count     = %" PRIu64 "", b_type);
-    Log("   - C(csr) type count      = %" PRIu64 "", c_type);
-    Log("   - Memory load type count     = %" PRIu64 "", load_type);
-    Log("   - Memory store type count    = %" PRIu64 "", store_type);
-    Log("CPI = %" PRIu64 "", cycle_count/inst_count);
-    Log("IPC = %lf", (double)inst_count/(double(cycle_count)));
-    Log("Total lsu read = %" PRIu64 "", lsu_read_count);
+    Log("Total cycle count = %" PRIu64, cycle_count);
+    Log("Total insts count = %" PRIu64, inst_count);
+    Log("   - A(alu) type count         = %" PRIu64 "(%.3lf)", a_type, ((double)a_type)/(double(inst_count)));
+    Log("   - B(branch) type count      = %" PRIu64 "(%.3lf)", b_type, ((double)b_type)/(double(inst_count)));
+    Log("   - C(csr) type count         = %" PRIu64 "(%.3lf)", c_type, ((double)c_type)/(double(inst_count)));
+    Log("   - Memory load type count    = %" PRIu64 "(%.3lf)", load_type, ((double)load_type)/(double(inst_count)));
+    Log("   - Memory store type count   = %" PRIu64 "(%.3lf)", store_type, ((double)store_type)/(double(inst_count)));
+    Log("CPI = %" PRIu64, cycle_count/inst_count);
+    Log("IPC = %lf", ((double)inst_count)/(double(cycle_count)));
+    Log("Total lsu read = %" PRIu64, lsu_read_count);
     Log("*****************************************");
 }
 #endif
