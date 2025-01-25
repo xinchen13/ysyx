203e63248149aad33991849d7c241fde518659dd
tracer-ysyx <tracer@ysyx.org>
2025-01-25 16:23:13 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:23:13 up 1 day,  6:54,  1 user,  load average: 0.66, 0.38, 0.38

--- npc/vsrc/perip/amba/apb_delayer.sv
+++ npc/vsrc/perip/amba/apb_delayer.sv
@@ -27,17 +27,6 @@ module apb_delayer(
 );
     `ifdef PMU_ON
 
-    assign out_paddr   = in_paddr;
-    assign out_psel    = in_psel;
-    assign out_penable = in_penable;
-    assign out_pprot   = in_pprot;
-    assign out_pwrite  = in_pwrite;
-    assign out_pwdata  = in_pwdata;
-    assign out_pstrb   = in_pstrb;
-    assign in_pready   = out_pready;
-    assign in_prdata   = out_prdata;
-    assign in_pslverr  = out_pslverr;
-
     // assign out_paddr   = in_paddr;
     // assign out_psel    = in_psel;
     // assign out_penable = in_penable;
@@ -45,77 +34,88 @@ module apb_delayer(
     // assign out_pwrite  = in_pwrite;
     // assign out_pwdata  = in_pwdata;
     // assign out_pstrb   = in_pstrb;
+    // assign in_pready   = out_pready;
+    // assign in_prdata   = out_prdata;
+    // assign in_pslverr  = out_pslverr;
 
-    // // states
-    // localparam IDLE         = 3'b000;
-    // localparam APB_ACTIVE   = 3'b001;
-    // localparam APB_DELAY    = 3'b010;
-    // localparam COFF_S       = 10;
-    // localparam COFF_R       = 64'd1;    // 0； no dalay    n: n delay
-    // // if device_clk=100MHz, COFF_R=9 means core_clk=1GHz
-    // localparam R_MUL_S      = COFF_R << COFF_S;
-    // reg [2:0] state;
-    // reg [63:0] delay_counter;
-    // reg  [31:0] temp_prdata;
-    // reg         temp_pslverr;
+    assign out_paddr   = in_paddr;
+    assign out_psel    = in_psel;
+    assign out_penable = in_penable;
+    assign out_pprot   = in_pprot;
+    assign out_pwrite  = in_pwrite;
+    assign out_pwdata  = in_pwdata;
+    assign out_pstrb   = in_pstrb;
 
-    // always @ (posedge clock) begin
-    //     if (reset) begin
-    //         state <= IDLE;
-    //     end
-    //     else begin
-    //         if(in_psel & !in_penable) begin
-    //             state <= APB_ACTIVE;
-    //         end
-    //         else if((state == APB_ACTIVE) & out_pready) begin
-    //             state <= APB_DELAY;
-    //         end
-    //         else if((state == APB_DELAY) & (delay_counter == 64'b0)) begin
-    //             state <= IDLE;
-    //         end
-    //     end
-    // end
+    // states
+    localparam IDLE         = 3'b000;
+    localparam APB_ACTIVE   = 3'b001;
+    localparam APB_DELAY    = 3'b010;
+    localparam COFF_S       = 10;
+    localparam COFF_R       = 64'd1;    // 0； no dalay    n: n delay
+    // if device_clk=100MHz, COFF_R=9 means core_clk=1GHz
+    localparam R_MUL_S      = COFF_R << COFF_S;
+    reg [2:0] state;
+    reg [63:0] delay_counter;
+    reg  [31:0] temp_prdata;
+    reg         temp_pslverr;
 
-    // always @ (posedge clock) begin
-    //     if (reset) begin
-    //         delay_counter <= 'b0;
-    //     end
-    //     else begin
-    //         if ((state == APB_ACTIVE) & (!out_pready)) begin
-    //             delay_counter <= delay_counter + R_MUL_S;
-    //         end
-    //         else if ((state == APB_ACTIVE) & out_pready) begin
-    //             delay_counter <= (delay_counter >> COFF_S);
-    //             temp_prdata <= out_prdata;
-    //             temp_pslverr <= out_pslverr;
-    //         end
-    //         else if ((state == APB_DELAY) & (delay_counter != 64'b0)) begin
-    //             delay_counter <= delay_counter - 64'b1;
-    //         end
-    //     end
-    // end
+    always @ (posedge clock) begin
+        if (reset) begin
+            state <= IDLE;
+        end
+        else begin
+            if(in_psel & !in_penable) begin
+                state <= APB_ACTIVE;
+            end
+            else if((state == APB_ACTIVE) & out_pready) begin
+                state <= APB_DELAY;
+            end
+            else if((state == APB_DELAY) & (delay_counter == 64'b0)) begin
+                state <= IDLE;
+            end
+        end
+    end
+
+    always @ (posedge clock) begin
+        if (reset) begin
+            delay_counter <= 'b0;
+        end
+        else begin
+            if ((state == APB_ACTIVE) & (!out_pready)) begin
+                delay_counter <= delay_counter + R_MUL_S;
+            end
+            else if ((state == APB_ACTIVE) & out_pready) begin
+                delay_counter <= (delay_counter >> COFF_S);
+                temp_prdata <= out_prdata;
+                temp_pslverr <= out_pslverr;
+            end
+            else if ((state == APB_DELAY) & (delay_counter != 64'b0)) begin
+                delay_counter <= delay_counter - 64'b1;
+            end
+        end
+    end
 
 
 
-    // always @ (posedge clock) begin
-    //     if (reset) begin
-    //         in_pready   <= 'b0;
-    //         in_prdata   <= 'b0;
-    //         in_pslverr  <= 'b0;
-    //     end
-    //     else begin
-    //         if ((state == APB_DELAY) & (delay_counter == 64'b0)) begin
-    //             in_pready   <= 'b1;
-    //             in_prdata   <= temp_prdata;
-    //             in_pslverr  <= temp_pslverr;
-    //         end
-    //         else begin
-    //             in_pready   <= 'b0;
-    //             in_prdata   <= 'b0;
-    //             in_pslverr  <= 'b0;
-    //         end
-    //     end
-    // end
+    always @ (posedge clock) begin
+        if (reset) begin
+            in_pready   <= 'b0;
+            in_prdata   <= 'b0;
+            in_pslverr  <= 'b0;
+        end
+        else begin
+            if ((state == APB_DELAY) & (delay_counter == 64'b0)) begin
+                in_pready   <= 'b1;
+                in_prdata   <= temp_prdata;
+                in_pslverr  <= temp_pslverr;
+            end
+            else begin
+                in_pready   <= 'b0;
+                in_prdata   <= 'b0;
+                in_pslverr  <= 'b0;
+            end
+        end
+    end
 
     `else
     assign out_paddr   = in_paddr;
