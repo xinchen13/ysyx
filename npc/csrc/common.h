2e3ee7227a5c60ee167f4cea0640104a4f592526
tracer-ysyx <tracer@ysyx.org>
2025-02-02 21:51:21 +0800

    > sim RTL ysyx_22040000 xinchen Linux sakura 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:21 up 1 day, 12:27,  1 user,  load average: 1.11, 0.84, 0.69

--- npc/csrc/common.h
+++ npc/csrc/common.h
@@ -9,8 +9,8 @@
 #define CONFIG_BATCH_MODE 1
 #define CONFIG_RVE 1
 #define CONFIG_XIP_FLASH 1
-#define CONFIG_PMU 1
-// #define CONFIG_ICACHE 1
+// #define CONFIG_PMU 1
+#define CONFIG_ICACHE 1
 // #define CONFIG_WATCHPOINT 1
 // #define CONFIG_ITRACE 1
 // #define CONFIG_FTRACE 1
